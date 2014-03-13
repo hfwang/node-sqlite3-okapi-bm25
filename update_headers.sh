65ae880c338ae504d5245fccae5d1ea175550544
Hsiu-Fan Wang <hfwang@porkbuns.net>
2014-03-12 23:53:40 -0700

    Add sqlite3.h

--- update_headers.sh
+++ update_headers.sh
@@ -3,4 +3,5 @@
 # This is a really really lame way of grabbing sqliteext.h
 npm install --build-from-source sqlite3
 cp ./node_modules/sqlite3/build/Release/obj/gen/sqlite-autoconf-*/sqlite3ext.h deps/sqlite3ext.h
+cp ./node_modules/sqlite3/build/Release/obj/gen/sqlite-autoconf-*/sqlite3.h deps/sqlite3.h
 rm -rf ./node_modules/sqlite3/
