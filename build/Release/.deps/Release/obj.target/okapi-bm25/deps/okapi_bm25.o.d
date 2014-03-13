10136c043d6fcbf2181e85ef72b39bf36676ef79
Hsiu-Fan Wang <hfwang@porkbuns.net>
2014-03-12 23:54:47 -0700

    get rid of build dir

--- build/Release/.deps/Release/obj.target/okapi-bm25/deps/okapi_bm25.o.d
+++
@@ -1,5 +0,0 @@
-cmd_Release/obj.target/okapi-bm25/deps/okapi_bm25.o := cc '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' -I/Users/darkwulf/.node-gyp/0.10.22/src -I/Users/darkwulf/.node-gyp/0.10.22/deps/uv/include -I/Users/darkwulf/.node-gyp/0.10.22/deps/v8/include -I../deps  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/okapi-bm25/deps/okapi_bm25.o.d.raw  -c -o Release/obj.target/okapi-bm25/deps/okapi_bm25.o ../deps/okapi_bm25.c
-Release/obj.target/okapi-bm25/deps/okapi_bm25.o: ../deps/okapi_bm25.c \
-  ../deps/sqlite3ext.h
-../deps/okapi_bm25.c:
-../deps/sqlite3ext.h:
